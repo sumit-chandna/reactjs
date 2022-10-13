7a93fc63cb86968e2aa178219704f65011e406f7
Chandna, Sumit K <sumit.chandna@optum.com>
2022-10-13 14:32:01 +0530

    Real-Time Validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,7 +3,19 @@ import Button from '../shared/Button';
 import Card from '../shared/Card';
 function FeedbackForm() {
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
     const handleTextChange = (e) => {
+        if (text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text Must be atleast 10 characters')
+        } else {
+            setBtnDisabled(false)
+            setMessage(null)
+        }
         setText(e.target.value);
     }
     return (
@@ -12,8 +24,9 @@ function FeedbackForm() {
                 <h2>How Would You Rate your service with us?</h2>
                 <div className='input-group'>
                     <input type="text" placeholder='Write a review' onChange={handleTextChange} value={text} />
-                    <Button type='submit'>Send</Button>
+                    <Button type='submit' isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className='message'>{message}</div>}
             </form>
         </Card>
     )
