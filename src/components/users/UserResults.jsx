2c469165ffb202fb7ba0c882a9d7fa57a480f076
Chandna, Sumit K <sumit.chandna@optum.com>
2022-10-20 14:33:42 +0530

    basic setup

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import GithubContext from '../../context/github/GithubContext';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+
+
+function UserResults() {
+    const { users, isLoading } = useContext(GithubContext);
+
+    if (!isLoading && (!users || users.length === 0)) {
+        return <p>No Users</p>
+    }
+    return isLoading ? <Spinner /> :
+        <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+            {users.map((user) => (
+                <UserItem key={user.id} user={user} />
+            ))}
+        </div>
+}
+
+export default UserResults
